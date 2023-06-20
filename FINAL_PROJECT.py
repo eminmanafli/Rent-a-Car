d0ef05100a722cbe4b1a9b0659fa5df899fab6e3
Emin Manafli <eminmanafli07@gmail.com>
2023-06-20 22:06:23 +0400

    searching function, new window(not ready) were added

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -11,6 +11,8 @@ class Car:
         self.buraxilis_ili = buraxilis_ili
         self.reng = reng
         self.nomre = nomre
+        self.rented = False
+        self.renter = None
 
     def __str__(self):
         return f'{self.marka} {self.model}'
@@ -30,8 +32,13 @@ class Musteri:
 def masinlar():
     def find():
         nonlocal cars
-        found_cars = list(filter(lambda car: find_cars(car, axtaris_entry), cars))
+        found_cars = list(filter(lambda car: find_cars(car, axtaris_entry, axtaris_filtr[axtaris_listbox.curselection()[0]]), cars))
         cars_var.set(found_cars)
+
+    def reset():
+        nonlocal cars
+        cars_var.set(cars)
+        axtaris_entry.delete(0, END)
 
     def edit():
         data_car = cars[cars_listbox.curselection()[0]]
@@ -49,10 +56,15 @@ def masinlar():
     def apply():
         change_car = cars[cars_listbox.curselection()[0]]
         change_car.marka = marka_entry.get()
+        marka_entry.delete(0, END)
         change_car.model = model_entry.get()
+        model_entry.delete(0, END)
         change_car.buraxilis_ili = il_entry.get()
+        il_entry.delete(0, END)
         change_car.reng = reng_entry.get()
+        reng_entry.delete(0, END)
         change_car.nomre = nomre_entry.get()
+        nomre_entry.delete(0, END)
         cars_var.set(cars)
 
     def delete():
@@ -158,12 +170,7 @@ def masinlar():
     apply_button = Button(master=masin, text='Apply', bg='purple', fg='white', font=20, width=10, command=apply)
     apply_button.place(x=400, y=500)
 
-    marka_filtr = 'Marka'
-    model_filtr = 'Model'
-    il_filtr = 'Il'
-    reng_filtr = 'Reng'
-    nomre_filtr = 'Nomre'
-    axtaris_filtr = [marka_filtr, model_filtr, il_filtr, reng_filtr, nomre_filtr]
+    axtaris_filtr = ['Marka', 'Model', 'Il', 'Reng', 'Nomre']
     axtaris_filtr_var = Variable(value=axtaris_filtr)
 
     axtaris_listbox = Listbox(master=masin, listvariable=axtaris_filtr_var, selectmode=SINGLE, fg='black', bg='white', font=20, selectbackground='blue', selectforeground='white')
@@ -178,13 +185,23 @@ def masinlar():
     axtaris_button = Button(master=masin ,text='Axtar', bg='pink', fg='black', font=20, width=10, command=find)
     axtaris_button.place(x=790, y=300)
 
-    reset_button = Button(master=masin, text='Reset', bg='lime', fg='black', font=20, width=10)
+    reset_button = Button(master=masin, text='Reset', bg='lime', fg='black', font=20, width=10, command=reset)
     reset_button.place(x=790, y=350)
 
     masin.mainloop()
 
 
 def musteriler():
+    def find():
+        nonlocal musteriler
+        found_people = list(filter(lambda musteri: find_musteriler(musteri, axtaris_entry, axtaris_filtr[axtaris_listbox.curselection()[0]]), musteriler))
+        musteriler_var.set(found_people)
+
+    def reset():
+        nonlocal musteriler
+        musteriler_var.set(musteriler)
+        axtaris_entry.delete(0, END)
+
     def edit():
         data_car = musteriler[musteriler_listbox.curselection()[0]]
         ad_entry.delete(0, END)
@@ -199,9 +216,13 @@ def musteriler():
     def apply():
         change_person = musteriler[musteriler_listbox.curselection()[0]]
         change_person.ad = ad_entry.get()
+        ad_entry.delete(0, END)
         change_person.model = soyad_entry.get()
+        soyad_entry.delete(0, END)
         change_person.unvan = unvan_entry.get()
+        unvan_entry.delete(0, END)
         change_person.telefon_nomresi = telefon_nomresi_entry.get()
+        telefon_nomresi_entry.delete(0, END)
         musteriler_var.set(musteriler)
 
     def delete():
@@ -246,7 +267,7 @@ def musteriler():
     musteriWord = Label(master=musteri, image=tk_musteriWordPhoto)
     musteriWord.place(x=0, y=0)
 
-    musteri1 = Musteri('Peter', 'Parker', 'USA', 1234567890)
+    musteri1 = Musteri('Peter', 'Parker', 'USA', '1234567890')
     musteriler = [musteri1]
     try:
         file = open('MusteriFile.txt', 'rb')
@@ -303,7 +324,62 @@ def musteriler():
     apply_button = Button(master=musteri, text='Apply', bg='purple', fg='white', font=20, width=10, command=apply)
     apply_button.place(x=400, y=500)
 
+    axtaris_filtr = ['Ad', 'Soyad', 'Unvan', 'Telefon']
+    axtaris_filtr_var = Variable(value=axtaris_filtr)
+
+    axtaris_listbox = Listbox(master=musteri, listvariable=axtaris_filtr_var, selectmode=SINGLE, fg='black', bg='white',
+                              font=20, selectbackground='blue', selectforeground='white')
+    axtaris_listbox.place(x=590, y=250, width=100, height=100)
+
+    axtaris_label = Label(master=musteri, text='Axtaris filtrleri:', font=('Helvetica', 20), fg='black')
+    axtaris_label.place(x=585, y=202)
+
+    axtaris_entry = Entry(master=musteri, width=15, font=20)
+    axtaris_entry.place(x=790, y=250)
+
+    axtaris_button = Button(master=musteri, text='Axtar', bg='pink', fg='black', font=20, width=10, command=find)
+    axtaris_button.place(x=790, y=300)
+
+    reset_button = Button(master=musteri, text='Reset', bg='lime', fg='black', font=20, width=10, command=reset)
+    reset_button.place(x=790, y=350)
+
     musteri.mainloop()
+
+
+def kiraye():
+    kiraye_window = Toplevel(root)
+    kiraye_window.title('Kiraye alib_verme')
+    kiraye_window.geometry('1000x600')
+    kiraye_window.iconbitmap('images/rent1.ico')
+    kiraye_window.maxsize(width=1000, height=600)
+    kiraye_window.minsize(width=1000, height=600)
+
+    kiraye_image = resize_image('images/rent4.png', 1000, 200)
+    kiraye_label = Label(master=kiraye_window, image=kiraye_image)
+    kiraye_label.place(x=0, y=0)
+
+    musteriler2 = []
+    try:
+        file = open('MusteriFile.txt', 'rb')
+        try:
+            musteriler2 = pickle.load(file)
+        except Exception as e:
+            print(e)
+        finally:
+            file.close()
+    except Exception as ex:
+        print(ex)
+    musteriler2_var = Variable(value=musteriler2)
+    musteri_secim = Listbox(master=kiraye_window, listvariable=musteriler2_var, selectmode=SINGLE, fg='black', bg='white',
+                              font=20, selectbackground='blue', selectforeground='white')
+    musteri_secim.place(x=0, y=250, width=200, height=150)
+    musteri_secim_scrollbar = Scrollbar(master=kiraye_window, highlightbackground='gray', highlightcolor='black', bg='gray',
+                              activebackground='black', orient='vertical', command=musteri_secim.yview)
+    musteri_secim_scrollbar.place(x=200, y=250)
+    musteri_secim['yscrollcommand'] = musteri_secim_scrollbar.set
+
+
+    kiraye_window.mainloop()
 
 
 def resize_image(image_path, width, height):
@@ -311,9 +387,39 @@ def resize_image(image_path, width, height):
     resized_image = original_image.resize((width, height), Image.LANCZOS)
     return ImageTk.PhotoImage(resized_image)
 
-def find_cars(car, entry):
-    if entry.get() in car.marka:
-        return car
+
+def find_cars(car, entry, selection):
+    if selection == 'Marka':
+        if entry.get() in car.marka:
+            return car
+    elif selection == 'Model':
+        if entry.get() in car.model:
+            return car
+    elif selection == 'Il':
+        if entry.get() in car.buraxilis_ili:
+            return car
+    elif selection == 'Reng':
+        if entry.get() in car.reng:
+            return car
+    elif selection == 'Nomre':
+        if entry.get() in car.nomre:
+            return car
+
+
+def find_musteriler(musteri, entry, selection):
+    if selection == 'Ad':
+        if entry.get() in musteri.ad:
+            return musteri
+    elif selection == 'Soyad':
+        if entry.get() in musteri.soyad:
+            return musteri
+    elif selection == 'Unvan':
+        if entry.get() in musteri.unvan:
+            return musteri
+    elif selection == 'Telefon':
+        if entry.get() in musteri.telefon_nomresi:
+            return musteri
+
 
 
 root = Tk()
@@ -331,16 +437,23 @@ emrler = Label(master=root, image=tk_emrler_photo,
                compound='center', font=('Helvetica', 30, 'bold'), fg='orange')
 emrler.place(x=0, y=0, width=1000, height=200)
 
-masinlar_photo = resize_image('images/car1.png', 500, 400)
+masinlar_photo = resize_image('images/car1.png', 350, 400)
 masinlar_button = Button(master=root, image=masinlar_photo, command=masinlar)
 masinlar_button.place(x=0, y=200)
 masinlar_label = Label(master=root, text='Maşınlar', font=('Helvetica', 20, 'bold'), fg='black', bg='#e1e1d7')
-masinlar_label.place(x=200, y=230)
+masinlar_label.place(x=125, y=230)
 
-musteri_photo = resize_image('images/musteri_pencere.png', 500, 400)
+musteri_photo = resize_image('images/musteri_pencere.png', 350, 400)
 musteri_button = Button(master=root, image=musteri_photo, command=musteriler)
-musteri_button.place(x=500, y=200)
+musteri_button.place(x=350, y=200)
 musteri_label = Label(master=root, text='Müştərilər', font=('Helvetica', 20, 'bold'), fg='black', bg='white')
-musteri_label.place(x=680, y=230)
+musteri_label.place(x=450, y=230)
+
+kiraye_photo = resize_image('images/rent.png', 300, 400)
+kiraye_button = Button(master=root, image=kiraye_photo, command=kiraye)
+kiraye_button.place(x=700, y=200)
+kiraye_label = Label(master=root, text='Kiraye\nalib-verme', font=('Helvetica', 20, 'bold'), fg='black', bg='#008B8B')
+kiraye_label.place(x=705, y=205)
+
 
 root.mainloop()
